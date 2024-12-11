9ae4bc2fe83171a8df746d49f295960b2f06da4e
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-12-11 14:30:11 +0530

    Create JwtConstant.java

---
+++ src/main/java/com/janith/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.janith.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "knfksndjfbwfoebrfjebrjbejrnqwjberjqwhoirhrjkebrjkebjrbjk3o2h43h4hrjneruherbewjrbhiugfbff";
+    public static final String JWT_HEADER = "Authorization";
+}
