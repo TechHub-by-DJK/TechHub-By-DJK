2ad184ef612826a86a653835dbe4c7b266b257df
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-12-12 10:27:57 +0530

    Create Shop.java

---
+++ src/main/java/com/janith/model/Shop.java
@@ -0,0 +1,53 @@
+package com.janith.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Shop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User owner;
+
+    private String name;
+
+    private String description;
+    private String buildingtype; //like cuisiontype ->Gaming, editing, normal office
+
+    @OneToOne
+    private Address address;
+
+    @Embedded
+    private ContactInformation contactIformation;
+
+    private String openingHours;
+
+    @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    @Column(length = 1000)
+    private List<String> images;
+
+    private LocalDateTime registrationDate;
+
+    private boolean open;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL)
+    private List<Computer> computers = new ArrayList<>();
+
+}
