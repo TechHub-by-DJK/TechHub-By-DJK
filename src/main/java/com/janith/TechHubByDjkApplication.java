a3dd741c3f1fa72fab7d23bd04675b2406f38e68
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-12-12 10:28:34 +0530

    Fix error with db
    Fix error with db

---
+++ src/main/java/com/janith/TechHubByDjkApplication.java
@@ -0,0 +1,13 @@
+package com.janith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHubByDjkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechHubByDjkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janith/TechHubByDjkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechHubByDjkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
