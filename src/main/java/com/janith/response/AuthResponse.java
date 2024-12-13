1056857162860f818521de5a04d6265403d7ffc9
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-12-13 08:56:28 +0530

    Create AuthResponse.java

---
+++ src/main/java/com/janith/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.janith.response;
+
+import com.janith.model.USER_ROLE;
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+    private String jwt;
+
+    private String message;
+
+    private USER_ROLE role;
+}
