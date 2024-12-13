867786bf394cb345ba7dff5ede30ef679bbf5a91
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-12-13 22:19:20 +0530

    Update AuthController.java

--- src/main/java/com/janith/controller/AuthController.java
+++ src/main/java/com/janith/controller/AuthController.java
@@ -92,7 +92,7 @@ public class AuthController {
 
         AuthResponse authResponse = new AuthResponse();
         authResponse.setJwt(jwt);
-        authResponse.setMessage("Registered Successfully");
+        authResponse.setMessage("Login Successfull");
         authResponse.setRole(USER_ROLE.valueOf(role));
 
         return new ResponseEntity<>(authResponse, HttpStatus.OK);
