f4a147f1c9dca5a4658a8f4753dd6cfda581a5dd
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-12-27 19:09:43 +0530

    Create CreateComputerRequest.java

---
+++ src/main/java/com/janith/request/CreateComputerRequest.java
@@ -0,0 +1,33 @@
+package com.janith.request;
+
+import com.janith.model.Category;
+import com.janith.model.IncludedComponents;
+import com.janith.model.Shop;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class CreateComputerRequest {
+    private String name;
+    private String description;
+    private Long price;
+
+    private Category category;
+    private List<String> images;
+    private  Long shopId;
+
+    private boolean isHomeUser;
+    private boolean isBusinessUser;
+    private boolean isGamer;
+    private boolean isDesigner;
+    private boolean isDeveloper;
+    private boolean isSeasonal;
+
+    private List<IncludedComponents> includedComponents;
+
+}
