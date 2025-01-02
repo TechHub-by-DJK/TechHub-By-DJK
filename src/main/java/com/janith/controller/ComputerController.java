b9f9b4d462e4ab879a66dbb5eb5a4c1885b366fc
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-01-02 16:10:12 +0530

    Create ComputerController.java

---
+++ src/main/java/com/janith/controller/ComputerController.java
@@ -0,0 +1,10 @@
+package com.janith.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/computer")
+public class ComputerController {
+}
