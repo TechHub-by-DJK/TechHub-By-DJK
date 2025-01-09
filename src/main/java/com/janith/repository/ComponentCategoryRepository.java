d8fe6fea09d23465e870a7cef47625d56e1bed7f
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-01-09 15:46:26 +0530

    Create ComponentCategoryRepository.java

---
+++ src/main/java/com/janith/repository/ComponentCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.janith.repository;
+
+import com.janith.model.ComponentCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComponentCategoryRepository extends JpaRepository<ComponentCategory, Long>{
+}
