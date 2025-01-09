18b6605c0f47d4d875309c4c8056816a2b7349ad
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-01-09 11:33:23 +0530

    Update CategoryController.java

--- src/main/java/com/janith/controller/CategoryController.java
+++ src/main/java/com/janith/controller/CategoryController.java
@@ -1,23 +1,44 @@
 package com.janith.controller;
 
 import com.janith.Service.CategoryService;
+import com.janith.Service.UserService;
 import com.janith.model.Category;
+import com.janith.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("api/admin/category")
+@RequestMapping("/api")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    public ResponseEntity<Category> createCategory(@RequestBody Category category,
-                                                   @RequestHeader("Authorized")){
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/admin/category")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category,
+                                                   @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+
+        Category createdCategory = categoryService.createCategory(category.getName(), user.getId());
+
+        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
     }
+
+    @GetMapping("/category/shop")
+    public ResponseEntity<List<Category>> getShopCategory(
+                                                   @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+
+        List<Category> categories = categoryService.findCategoryByShopId(user.getId());
+
+        return new ResponseEntity<>(categories, HttpStatus.CREATED);
+    }
+
 }
