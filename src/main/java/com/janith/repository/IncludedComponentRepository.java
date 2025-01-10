04327a5946a6fbe4227c62a9a74ef99e7eb52b27
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-01-10 09:49:32 +0530

    Update IncludedComponentRepository.java

--- src/main/java/com/janith/repository/IncludedComponentRepository.java
+++ src/main/java/com/janith/repository/IncludedComponentRepository.java
@@ -3,5 +3,9 @@ package com.janith.repository;
 import com.janith.model.IncludedComponents;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IncludedComponentRepository extends JpaRepository<IncludedComponents, Long> {
+
+    List<IncludedComponents> findByShopId(Long shopId);
 }
