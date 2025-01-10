494d520f7cb753a6fb747e4b19a837d587f24f59
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-01-10 09:49:29 +0530

    Update ComponentCategoryRepository.java

--- src/main/java/com/janith/repository/ComponentCategoryRepository.java
+++ src/main/java/com/janith/repository/ComponentCategoryRepository.java
@@ -3,5 +3,9 @@ package com.janith.repository;
 import com.janith.model.ComponentCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ComponentCategoryRepository extends JpaRepository<ComponentCategory, Long>{
+
+    List<ComponentCategory> findByShopId(Long id);
 }
