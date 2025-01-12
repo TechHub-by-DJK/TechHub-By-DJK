12a98b5ebb0bb94801e5bbb4fca4d16e32c0180f
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-01-12 18:35:10 +0530

    Create CartServiceImp.java

---
+++ src/main/java/com/janith/Service/CartServiceImp.java
@@ -0,0 +1,125 @@
+package com.janith.Service;
+
+import com.janith.model.Cart;
+import com.janith.model.CartItem;
+import com.janith.model.Computer;
+import com.janith.model.User;
+import com.janith.repository.CartItemRepository;
+import com.janith.repository.CartRepository;
+import com.janith.repository.ComputerRepository;
+import com.janith.request.AddCartItemRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class CartServiceImp implements CartService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
+    @Autowired
+    private ComputerService computerService;
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public CartItem addItemToCart(AddCartItemRequest req, String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+
+        Computer computer = computerService.findComputerById(req.getComputerId());
+
+        Cart cart = cartRepository.findByCustomerId(user.getId());
+
+        for (CartItem cartItem : cart.getItem()){
+            if(cartItem.getComputer().equals(computer)){
+                int newQuantity = cartItem.getQuantity() + req.getQuantity();
+                return updateCartItemQuantity(cartItem.getId(), newQuantity);
+            }
+        }
+
+        CartItem newCartItem = new CartItem();
+        newCartItem.setComputer(computer);
+        newCartItem.setCart(cart);
+        newCartItem.setQuantity(req.getQuantity());
+        newCartItem.setComponents(req.getIncludedComponents());
+        newCartItem.setTotalPrice(req.getQuantity()*computer.getPrice());
+
+        CartItem savedCartItem = cartItemRepository.save(newCartItem);
+
+        cart.getItem().add(savedCartItem);
+
+        return savedCartItem;
+    }
+
+    @Override
+    public CartItem updateCartItemQuantity(Long cartItemId, int quantity) throws Exception {
+        Optional<CartItem> cartItemOptional = cartItemRepository.findById(cartItemId);
+        if(cartItemOptional.isEmpty()){
+            throw new Exception("CartItem not found");
+        }
+        CartItem cartItem = cartItemOptional.get();
+        cartItem.setQuantity(quantity);
+        cartItem.setTotalPrice(cartItem.getComputer().getPrice()*quantity);
+
+        return cartItemRepository.save(cartItem);
+    }
+
+    @Override
+    public Cart removeItemFromCart(Long cartItemId, String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        Cart cart = cartRepository.findByCustomerId(user.getId());
+
+        Optional<CartItem> cartItemOptional = cartItemRepository.findById(cartItemId);
+        if(cartItemOptional.isEmpty()){
+            throw new Exception("CartItem not found");
+        }
+
+        CartItem cartItem = cartItemOptional.get();
+
+        cart.getItem().remove(cartItem);
+
+        return cartRepository.save(cart);
+    }
+
+    @Override
+    public Long calculateCartTotals(Cart cart) throws Exception {
+        Long cartTotals = 0L;
+        for (CartItem cartItem : cart.getItem()) {
+            cartTotals += cartItem.getComputer().getPrice()*cartItem.getQuantity();
+        }
+        return cartTotals;
+    }
+
+    @Override
+    public Cart findCartById(Long id) throws Exception {
+        Optional<Cart> cartOptional = cartRepository.findById(id);
+        if(cartOptional.isEmpty()){
+            throw new Exception("CartItem not found with id: " + id);
+        }
+        return cartOptional.get();
+    }
+
+    @Override
+    public Cart findCartByUserId(String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+
+        return cartRepository.findByCustomerId(user.getId());
+    }
+
+    @Override
+    public Cart clearCart(String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        Cart cart = findCartByUserId(jwt);
+        cart.getItem().clear();
+        return cartRepository.save(cart);
+    }
+}
