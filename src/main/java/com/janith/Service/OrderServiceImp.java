39fd333af287a1674d6e4119b7ce5bfa4511cfd0
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-02-13 15:42:32 +0530

    Update OrderServiceImp.java

--- src/main/java/com/janith/Service/OrderServiceImp.java
+++ src/main/java/com/janith/Service/OrderServiceImp.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImp implements OrderService{
@@ -55,26 +58,72 @@ public class OrderServiceImp implements OrderService{
 
         Cart cart = cartService.findCartByUserId(user.getId());
 
-        return null;
+        List<OrderItem> orderItems  = new ArrayList<>();
+
+        for(CartItem cartItem : cart.getItem()){
+            OrderItem orderItem = new OrderItem();
+            orderItem.setComputer(cartItem.getComputer());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.setComponents(cartItem.getComponents());
+            orderItem.setTotalPrice(cartItem.getTotalPrice());
+
+            OrderItem savedOrderItem = orderItemRepository.save(orderItem);
+            orderItems.add(savedOrderItem);
+        }
+        Long totalPrice = cartService.calculateCartTotals(cart);
+
+        createdOrder.setItems(orderItems);
+        createdOrder.setTotalPrice(totalPrice);
+
+        Order savedOrder = orderRepository.save(createdOrder);
+        shop.getOrders().add(savedOrder);
+
+        return createdOrder;
     }
 
     @Override
     public Order updateOrder(Long orderId, String orderStatus) throws Exception {
-        return null;
+        Order order= findOrderById(orderId);
+
+        if(orderStatus.equals("OUT_FOR_DELIVERY") || orderStatus.equals("DELIVERED") || orderStatus.equals("PENDING") || orderStatus.equals("COMPLETED")) {
+            order.setOrderStatus(orderStatus);
+            return orderRepository.save(order);
+        }
+        throw new Exception("Please select a valid order status");
     }
 
     @Override
     public void cancelOrder(Long orderId) throws Exception {
 
+        Order order = findOrderById(orderId);
+        orderRepository.deleteById(orderId);
+
     }
 
     @Override
     public List<Order> getUsersOrder(Long userId) throws Exception {
-        return List.of();
+
+        return orderRepository.findByCustomerId(userId);
     }
 
     @Override
     public List<Order> getShopsOrder(Long shopId, String orderStatus) throws Exception {
-        return List.of();
+        List<Order> orders = orderRepository.findByShopId(shopId);
+
+        if (orderStatus!=null){
+            orders = orders.stream().filter(order -> order.getOrderStatus().equals(orderStatus)).collect(Collectors.toList());
+        }
+
+        return orders;
+    }
+
+    @Override
+    public Order findOrderById(Long orderId) throws Exception {
+        Optional<Order> optionalOrder = orderRepository.findById(orderId);
+
+        if(optionalOrder.isEmpty()) {
+            throw new Exception("Order not found");
+        }
+        return optionalOrder.get();
     }
 }
